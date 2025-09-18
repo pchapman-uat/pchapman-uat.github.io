67cf044116897b763b0d2155e4ec0c36e54c01c1
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-09-17 17:27:56 -0700

    Add Repo Card

--- src/elements/ProjectPage.tsx
+++ src/elements/ProjectPage.tsx
@@ -1,14 +1,21 @@
-import { ReactNode } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import { ProjectObj } from "@/classes/Projects";
 import Shield from "./Shield";
 import ProjectPageCSS from "@/style/projects.page.module.css";
 import { ProjectLinkElement } from "./ProjectElement";
+import { RepoCardProps } from "react-repo-card";
 export type ProjectPageParams = {
   project: ProjectObj;
   children?: ReactNode;
 };
 
 export default function ProjectPage({ project, children }: ProjectPageParams) {
+  const [RepoCard, setRepoCard] = useState<FC<RepoCardProps> | null>(null);
+  useEffect(() => {
+    import("react-repo-card").then((mod) => {
+      setRepoCard(() => mod.default);
+    });
+  }, []);
   return (
     <>
       <h3>{project.NAME}</h3>
@@ -21,6 +28,15 @@ export default function ProjectPage({ project, children }: ProjectPageParams) {
           />
         ))}
       </div>
+      <section>
+        {RepoCard && project.GITHUB && (
+          <RepoCard
+            username={project.GITHUB.user}
+            repository={project.GITHUB.repo}
+            dark
+          />
+        )}
+      </section>
       <div className={ProjectPageCSS.shields}>
         {project.GITHUB && (
           <>
