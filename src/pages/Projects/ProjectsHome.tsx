1d71d4c039efc30b9567507be02759abe0c5d17d
Preston Chapman <144168412+pchapman-uat@users.noreply.github.com>
2025-10-06 10:49:26 -0700

    Add JS Div

--- src/pages/Projects/ProjectsHome.tsx
+++ src/pages/Projects/ProjectsHome.tsx
@@ -1,23 +1,22 @@
 import { ALL_PROJECTS } from "@/classes/Projects";
+import JSDiv from "@/elements/JSDiv";
 import ProjectElement from "@/elements/ProjectElement";
 import ProjectCSS from "@/style/projects.module.css";
 import { useEffect, useState } from "react";
 export default function ProjectsHome() {
   const [filter, setFilter] = useState<string>("");
-  const [jsEnabled, setJsEnabled] = useState(false);
-  useEffect(() => {
-    setJsEnabled(true);
-  }, []);
+
   return (
     <>
-      {jsEnabled && (
+      <JSDiv fallback={null}>
         <div>
           <input
             placeholder="Search"
             onChange={(event) => setFilter(event.target.value)}
           />
         </div>
-      )}
+      </JSDiv>
+
       <div className={ProjectCSS.projectsContainer}>
         {ALL_PROJECTS.map(
           (project, i) =>
